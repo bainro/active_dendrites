dc8b23bce7e78710dcd5a71947c8e91491c6170a
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-23 15:41:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import dendritic_mlp as D
 
 import torch
-      
+
+# batch_size=256,
+# val_batch_size=512,
+# tasks_to_validate=[1, 4, 9, 24, 49, 99],
+# seed=42,
+# loss_function=torch.nn.functional.cross_entropy,
+# optimizer_class=torch.optim.Adam
+
 conf = dict(
       input_size=784,
       output_size=10,  # Single output head shared by all tasks
