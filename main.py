ba1350edd4501a7e8da94adb2ee14b9fb91bbb61
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-23 16:49:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dendritic_mlp as D
 
 import torch
+from torch.utils.data import DataLoader
 
 # batch_size=256,
 # val_batch_size=512,
@@ -20,14 +21,27 @@ conf = dict(
     weight_sparsity=0.5,
     context_percent_on=0.1,
     num_segments=10
-)
+)    
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = D.DendriticMLP(**conf)
     model = model.to(device)
 
-    self.train_loader = self.create_train_dataloader(config)
+    if dataset is None:
+        dataset = cls.load_dataset(config, train=True)
+
+    sampler = cls.create_train_sampler(config, dataset)
+    
+    train_loader = DataLoader(
+        dataset=dataset,
+        batch_size=config.get("batch_size", 1),
+        shuffle=sampler is None,
+        num_workers=config.get("workers", 0),
+        sampler=sampler,
+        pin_memory=torch.cuda.is_available(),
+        drop_last=config.get("train_loader_drop_last", True),
+    )
 
     # import pdb; pdb.set_trace()
     print("SCRIPT FINISHED!")
