1d2fa597212cceceeae2d73b74e99e9af7faa963
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-23 16:45:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,24 @@ import torch
 # optimizer_class=torch.optim.Adam
 
 conf = dict(
-      input_size=784,
-      output_size=10,  # Single output head shared by all tasks
-      hidden_sizes=[2048, 2048],
-      dim_context=784,
-      kw=True,
-      kw_percent_on=0.05,
-      dendrite_weight_sparsity=0.0,
-      weight_sparsity=0.5,
-      context_percent_on=0.1,
-      num_segments=10
+    input_size=784,
+    output_size=10,  # Single output head shared by all tasks
+    hidden_sizes=[2048, 2048],
+    dim_context=784,
+    kw=True,
+    kw_percent_on=0.05,
+    dendrite_weight_sparsity=0.0,
+    weight_sparsity=0.5,
+    context_percent_on=0.1,
+    num_segments=10
 )
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = D.DendriticMLP(**conf)
     model = model.to(device)
+
+    self.train_loader = self.create_train_dataloader(config)
 
     # import pdb; pdb.set_trace()
     print("SCRIPT FINISHED!")
