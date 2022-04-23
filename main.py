b9c71ab17d3777d2edfc983ccbb383dd4dc6d6b9
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-23 15:41:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,8 @@ conf = dict(
 
 
       # batch_size=256,
-      val_batch_size=512,
-      tasks_to_validate=[1, 4, 9, 24, 49, 99],
-      distributed=False,
+      # val_batch_size=512,
+      # tasks_to_validate=[1, 4, 9, 24, 49, 99],
       seed=42,
 
       loss_function=torch.nn.functional.cross_entropy,
