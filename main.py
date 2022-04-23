fd9662798e93e6116a53305de847c39da8bb08e1
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-23 15:25:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import dendritic_mlp as DendriticMLP
+import dendritic_mlp as D
 
 import torch
       
@@ -28,4 +28,4 @@ optimizer_class=torch.optim.Adam
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = DendriticMLP().to(device)
+    model = D.DendriticMLP().to(device)
