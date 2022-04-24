9777ce3fc3ed1b29fae89b05c2bca4909935f277
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-24 11:50:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,8 @@ if __name__ == "__main__":
     class_indices = defaultdict(list)
     for idx, (_, target) in enumerate(dataset):
         class_indices[target].append(idx)
+        
+    print(class_indices);exit()
 
     # task -> all indices corresponding to this task
     task_indices = defaultdict(list)
