ca2feac4e9839589930bc111ce3834c644c8e9c8
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-23 22:16:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -93,10 +93,10 @@ if __name__ == "__main__":
         
         #print("type of imgs[0]: ", type(imgs[0]))
         #print(imgs[0])
-        #t = imgs[0].numpy()
-        #visual_test = numpy.transpose(t, (1, 2, 0))
-        #plt.imshow(visual_test, cmap='gray', vmin=0.4242, vmax=2.8215)
-        #plt.savefig('my_plot.png')
+        t = imgs[0].numpy()
+        visual_test = numpy.transpose(t, (1, 2, 0))
+        plt.imshow(visual_test, cmap='gray', vmin=0.4242, vmax=2.8215)
+        plt.savefig('my_plot_' + batch_idx + '.png')
         # break
 
     # import pdb; pdb.set_trace()
