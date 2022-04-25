514bec56068abdbe7e83fed7cf9af35cb7d1a072
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-24 18:30:12 -0700

    removed non-shared targets/labels code

--- datasets/permutedMNIST.py
+++ datasets/permutedMNIST.py
@@ -61,7 +61,7 @@ class PermutedMNIST(MNIST):
 
         # Since target values are not shared between tasks, `target` should be in the
         # range [0 + 10 * task_id, 9 + 10 * task_id]
-        target += 10 * task_id
+        # target += 10 * task_id
         return img, target
 
     def __len__(self):
