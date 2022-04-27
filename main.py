e2420096bed15a5965f202b4c1826af03c7b195a
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-27 15:58:07 -0700

    replaced dataloaders w/ fx code

--- main.py
+++ main.py
@@ -3,27 +3,21 @@ Script to train an active dendrite MLP on 10 tasks of PermutedMNIST.
 '''
 
 import os
-import math
-from collections import defaultdict
 
 import dendritic_mlp as D
-from datasets.permutedMNIST import PermutedMNIST
-from samplers import TaskRandomSampler
+from datasets.permutedMNIST import PermutedMNIST, make_loader
 
 import numpy
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
 seed = 44
 num_epochs = 3
-batch_size = 256
-test_batch_size = 512
+train_bs = 256
+test_bs = 512
 num_tasks = 10
-num_classes = 10 * num_tasks
-num_classes_per_task = math.floor(num_classes / num_tasks)
 
 conf = dict(
     input_size=784,
@@ -43,71 +37,9 @@ if __name__ == "__main__":
     model = D.DendriticMLP(**conf)
     model = model.to(device)
     
-    dataset = PermutedMNIST(
-        root=os.path.expanduser("~/datasets/permutedMNIST"),
-        download=False,  # Change to True if running for the first time
-        seed=seed,
-        train=True,
-        num_tasks=num_tasks,
-    )
     
-    # target -> all indices for that target
-    class_indices = defaultdict(list)
-    for idx in range(len(dataset)):
-        target = int(dataset.targets[idx % len(dataset.data)])
-        task_id = dataset.get_task_id(idx)
-        target += 10 * task_id
-        class_indices[target].append(idx)
-
-    # task -> all indices corresponding to this task
-    task_indices = defaultdict(list)
-    for i in range(num_tasks):
-        for j in range(num_classes_per_task):
-            task_indices[i].extend(class_indices[j + (i * num_classes_per_task)])
-
-    sampler = TaskRandomSampler(task_indices)
-    
-    train_loader = DataLoader(
-        dataset=dataset,
-        batch_size=batch_size,
-        shuffle=sampler is None,
-        num_workers=4,
-        sampler=sampler,
-        pin_memory=torch.cuda.is_available(),
-        drop_last=True,
-    )
-    
-    test_dataset = PermutedMNIST(
-        root=os.path.expanduser("~/datasets/permutedMNIST"),
-        download=False,
-        seed=seed,
-        train=False,
-        num_tasks=num_tasks,
-    )
-    
-    test_class_indices = defaultdict(list)
-    for idx in range(len(test_dataset)):
-        target = int(test_dataset.targets[idx % len(test_dataset.data)])
-        task_id = test_dataset.get_task_id(idx)
-        target += 10 * task_id
-        test_class_indices[target].append(idx)
-
-    test_task_indices = defaultdict(list)
-    for i in range(num_tasks):
-        for j in range(num_classes_per_task):
-            test_task_indices[i].extend(test_class_indices[j + (i * num_classes_per_task)])
-
-    test_sampler = TaskRandomSampler(test_task_indices)
-    
-    test_loader = DataLoader(
-        dataset=test_dataset,
-        batch_size=test_batch_size,
-        shuffle=test_sampler is None,
-        num_workers=4,
-        sampler=test_sampler,
-        pin_memory=torch.cuda.is_available(),
-        drop_last=False,
-    )
+    dataset, train_loader = make_loader(num_tasks, seed, train_bs, train=True)
+    test_dataset, test_loader = make_loader(num_tasks, seed, test_bs, train=False)
     
     # Optimizer and Loss
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, weight_decay=0)
@@ -119,24 +51,16 @@ if __name__ == "__main__":
             model.train()
             for batch_idx, (imgs, targets) in enumerate(train_loader):
                 optimizer.zero_grad()
-
                 imgs, targets = imgs.to(device), targets.to(device)
-
-                # @TODO this context vector is diff than the paper
-                # but can also double check that task 0's training loss is still low after training task 1, etc
-                # might be even easier to just try out 3-layer MLP, since that's still supposed to get like 80%
                 one_hot_vector = torch.zeros([num_tasks])
                 one_hot_vector[curr_task] = 1
                 context = torch.FloatTensor(one_hot_vector)
                 context = context.to(device)
                 context = context.unsqueeze(0)
                 context = context.repeat(imgs.shape[0], 1)
-
                 imgs = imgs.flatten(start_dim=1)
                 output = model(imgs, context)
                 pred = output.data.max(1, keepdim=True)[1]
-                # print(f"targets: {targets[10, ...]}")
-                # print(f"predictions: {pred[10, ...]}")
                 train_loss = criterion(output, targets)
                 train_loss.backward()
                 # print(f"train_loss: {train_loss.item()}")
@@ -158,8 +82,6 @@ if __name__ == "__main__":
                     imgs = imgs.flatten(start_dim=1)
                     output = model(imgs, context)
                     pred = output.data.max(1, keepdim=True)[1]
-                    # print(f"targets: {targets[10, ...]}")
-                    # print(f"predictions: {pred[10, ...]}")
                     correct += pred.eq(targets.data.view_as(pred)).sum().item()
             print(f"correct: {correct}")
             acc = 100. * correct * num_tasks / (curr_task+1) / len(test_loader.dataset)
