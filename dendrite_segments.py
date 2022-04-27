2bcda1c73d5611a828b57c9864d6ab70d5c23397
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-27 16:00:18 -0700

    Update dendrite_segments.py

--- dendrite_segments.py
+++ dendrite_segments.py
@@ -1,24 +1,3 @@
-# ----------------------------------------------------------------------
-# Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2020, Numenta, Inc.  Unless you have an agreement
-# with Numenta, Inc., for a separate license for this software code, the
-# following terms and conditions apply:
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero Public License version 3 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Affero Public License for more details.
-#
-# You should have received a copy of the GNU Affero Public License
-# along with this program.  If not, see http://www.gnu.org/licenses.
-#
-# http://numenta.org/licenses/
-# ----------------------------------------------------------------------
-
 """
 A simple implementation of dendrite segments. This is meant to offer, at the least,
 a template for revision and further modifications.
@@ -27,11 +6,9 @@ a template for revision and further modifications.
 import math
 from collections.abc import Iterable
 from itertools import product
-
 import numpy as np
 import torch
 from torch.nn import init
-
 from sparse_weights import HasRezeroWeights
 
 
