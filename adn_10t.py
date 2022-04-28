769ea651f15bb9e47517d72cc9ac91bcc0919921
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-28 16:13:03 -0700

    Update adn_10t.py

--- adn_10t.py
+++ adn_10t.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     model = D.DendriticMLP(**conf)
     model = model.to(device)    
     
-    dataset, train_loader = make_loader(num_tasks, seed, train_bs, train=True)
-    test_dataset, test_loader = make_loader(num_tasks, seed, test_bs, train=False)
+    train_loader = make_loader(num_tasks, seed, train_bs, train=True)
+    test_loader = make_loader(num_tasks, seed, test_bs, train=False)
     
     # Optimizer and Loss
     optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, weight_decay=0)
