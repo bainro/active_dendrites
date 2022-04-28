8c0442006a153e249fc4322de1642e7c57709fa4
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-28 16:40:41 -0700

    Update splitCIFAR100.py

--- datasets/splitCIFAR100.py
+++ datasets/splitCIFAR100.py
@@ -1,11 +1,12 @@
 import os
+import random
 from collections import defaultdict
 from samplers import TaskRandomSampler
 import numpy
 import torch
 from torchvision import transforms
 from torchvision.datasets import CIFAR100
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 
 def make_loader(seed, batch_size, train):
@@ -13,28 +14,42 @@ def make_loader(seed, batch_size, train):
     CIFAR-100 split into 10-way classification tasks. 
     """
     
-    # load regular 100 class dataset
-    # split into 10 tasks given a seed https://stackoverflow.com/questions/47432168/taking-subsets-of-a-pytorch-dataset
-    # return list of dataloaders
-    
     num_classes = 100
     num_classes_per_task = 10
     
-    dataset = CIFAR100(
+    whole_dataset = CIFAR100(
         root=os.path.expanduser("~/datasets/CIFAR100"),
         download=False,  # Change to True if running for the first time
         seed=seed,
         train=train,
     )
     
-    loader = DataLoader(
-        dataset=dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=4,
-        sampler=None,
-        pin_memory=torch.cuda.is_available(),
-        drop_last=train,
-    )
+    # load regular 100 class dataset
+    whole_loader = DataLoader(whole_dataset, batch_size=1, shuffle=True, num_workers=8)
     
-    return loader
+    # deterministically shuffle the tasks' classes
+    all_classes = list(range(0,100))
+    random.Random(seed).shuffle(all_classes)
+    
+    # list of dataloaders. One for each task.
+    loaders = []
+    for label_subset in all_labels:
+        subset_idx = []
+        for idx, (img, target) in enumerate(whole_loader):
+            if target in label_subset:
+                subset_idx.append(idx)
+        dataset_subset = Subset(whole_dataset, subset_idx)
+        loader = DataLoader(
+            dataset=dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=4,
+            sampler=None,
+            pin_memory=torch.cuda.is_available(),
+            drop_last=train,
+        )
+        loaders.append(loader)
+        
+        del whole_loader
+    
+    return loaders
