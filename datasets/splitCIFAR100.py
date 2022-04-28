0a90b2a479cc6112cc942c51926a715de5841378
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-28 16:14:08 -0700

    Update splitCIFAR100.py

--- datasets/splitCIFAR100.py
+++ datasets/splitCIFAR100.py
@@ -8,68 +8,24 @@ from torchvision.datasets import CIFAR100
 from torch.utils.data import DataLoader
 
 
-class splitCIFAR100(CIFAR100):
+def make_loader(seed, batch_size, train):
     """
     CIFAR-100 split into 10-way classification tasks. 
-    
     """
-
-    def __init__(self, num_tasks, seed, train, root=".", download=False):
-
-        t = [transforms.ToTensor()]
-        data_transform = transforms.Compose(t)
-        super().__init__(root=root, train=train, transform=data_transform,
-                         target_transform=None, download=download)
-
-        '''
-        # Use a generator object to manually set the seed and generate the same
-        # num_tasks splits for both training and validation datasets
-        g = torch.manual_seed(seed)
-
-        self.shuffled_classes = [
-            torch.randperm(784, generator=g) for task_id in range(1, num_tasks)
-        ]
-        '''
-
-    def __getitem__(self, index):        
-        img, target = super().__getitem__(index % len(self.data))
-        # Determine the which task `index` belongs to
-        task_id = self.get_task_id(index)
-        return img, target
     
-    @property
-    def processed_folder(self):
-        return os.path.join(self.root, "CIFAR100", "processed")
-
-    def get_task_id(self, index):
-        return index // len(self.data)
-
-def make_loader(seed, batch_size, train):
+    # load regular 100 class dataset
+    # split into 10 tasks given a seed
+    # replace sampler with list of datasets?
+    
     num_classes = 100
     num_classes_per_task = 10
     
-    dataset = splitCIFAR100(
-        root=os.path.expanduser("~/datasets/splitCIFAR100"),
-        download=True,  # Change to True if running for the first time
+    dataset = CIFAR100(
+        root=os.path.expanduser("~/datasets/CIFAR100"),
+        download=False,  # Change to True if running for the first time
         seed=seed,
         train=train,
     )
-    
-    # target -> all indices for that target
-    class_indices = defaultdict(list)
-    for idx in range(len(dataset)):
-        target = int(dataset.targets[idx % len(dataset.data)])
-        task_id = dataset.get_task_id(idx)
-        target += 10 * task_id
-        class_indices[target].append(idx)
-
-    # task -> all indices corresponding to this task
-    task_indices = defaultdict(list)
-    for i in range(num_tasks):
-        for j in range(num_classes_per_task):
-            task_indices[i].extend(class_indices[j + (i * num_classes_per_task)])
-
-    sampler = TaskRandomSampler(task_indices)
     
     loader = DataLoader(
         dataset=dataset,
@@ -81,4 +37,4 @@ def make_loader(seed, batch_size, train):
         drop_last=train,
     )
     
-    return dataset, loader
+    return loader
