c6b87c255224f58a1cb63f5d443832abe7e3fd63
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-28 15:28:58 -0700

    Update 3l_mlp_10t.py

--- 3l_mlp_10t.py
+++ 3l_mlp_10t.py
@@ -3,7 +3,7 @@ Script to train a fully connected, 3 layer MLP on 10 tasks of PermutedMNIST.
 '''
 
 import os
-from datasets.permutedMNIST import PermutedMNIST, make_loader
+from datasets.permutedMNIST import make_loader
 import numpy
 import torch
 from torch import nn
