3f42c180426dc8a3e15d39d7f63ba81efa72a078
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-28 18:14:11 -0700

    Update splitCIFAR100.py

--- datasets/splitCIFAR100.py
+++ datasets/splitCIFAR100.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from samplers import TaskRandomSampler
 import numpy
 import torch
-from torchvision import transforms
+from torchvision import transforms as trans
 from torchvision.datasets import CIFAR100
 from torch.utils.data import DataLoader, Subset
 
@@ -16,8 +16,8 @@ def make_loaders(seed, batch_size, train):
     data to be of the same classes as training.
     """
 
-    t = transforms.Compose([transforms.ToTensor(), 
-                           ])#transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    t = trans.Compose([trans.ToTensor(),])
+                       # trans.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
     
     conf = {"root": os.path.expanduser("~/datasets/CIFAR100"),
             "download": False, "train": train, "transform": t}
