b421ea66ff1a7708ceac2e837ddc3660fb1f46c9
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-28 22:51:50 -0700

    Update grid_search_cnn.py

--- grid_search_cnn.py
+++ grid_search_cnn.py
@@ -4,16 +4,21 @@ if __name__ == "__main__":
     LRs = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4]
     BSs = [64, 128, 256, 512]
     decays = [0, 1e-5, 5e-5, 1e-4]
-    seed = 42
+    seeds = [42, 69, 420] # lol
     
     for lr in LRs:
         for bs in BSs:
             for w_d in decays:
-                conf = {"seed": seed, "train_bs": bs, 
-                        "lr": lr, "w_decay": w_d}
-                avg_acc, final_epochs, final_single_acc = train(**conf)
-                print(**conf)
-                print(f"avg_acc: {avg_acc}%")
-                print(f"final_epochs: {final_epochs}") 
-                print(f"final_single_acc: {final_single_acc}")
+                per_seed_acc = []
+                for seed in seeds:
+                    conf = {"seed": seed, "train_bs": bs, 
+                            "lr": lr, "w_decay": w_d}
+                    avg_acc, final_epochs, final_single_acc = train(**conf)
+                    per_seed_acc.append(avg_acc)
+                    print(**conf)
+                    print(f"avg_acc: {avg_acc}%")
+                    print(f"final_epochs: {final_epochs}") 
+                    print(f"final_single_acc: {final_single_acc}")
+                avg_across_seeds = sum(per_seed_acc) / len(per_seed_acc)
+                print(f"seed avg acc: {avg_across_seeds}")
                 print("\n\n")
