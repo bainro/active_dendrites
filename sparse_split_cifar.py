d0403d1809034e7ac60a5738caaae2d6a5134aea
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-29 23:16:09 -0700

    Update sparse_split_cifar.py

--- sparse_split_cifar.py
+++ sparse_split_cifar.py
@@ -15,7 +15,7 @@ num_epochs = 1000
 test_bs = 512
 test_freq = 5
 num_tasks = 10
-tolerance = test_freq * 12
+tolerance = test_freq * 6
 
 class LeNet5(nn.Module):
     def __init__(self, num_classes=10):
