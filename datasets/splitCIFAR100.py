a7ae868ad22acb289dccb6d7f61cd30239c83a96
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-04-30 19:02:18 -0700

    Update splitCIFAR100.py

--- datasets/splitCIFAR100.py
+++ datasets/splitCIFAR100.py
@@ -83,8 +83,9 @@ def make_loaders(seed, batch_size, train):
             drop_last=train,
         )
         loaders.append(loader)
+        print(next(iter(loader))[1])
     #print(whole_dataset.targets)
-    #exit()
+    exit()
     del whole_loader
     
     return loaders
