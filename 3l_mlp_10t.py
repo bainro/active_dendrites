e8be7241339e4f243e8e590a82277e199e67b87b
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-01 13:36:27 -0700

    multiple seeds

--- 3l_mlp_10t.py
+++ 3l_mlp_10t.py
@@ -63,45 +63,56 @@ class ModifiedInitMLP(nn.Module):
 
     
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = ModifiedInitMLP(**conf)
-    model = model.to(device)
-    
-    train_loader = make_loader(num_tasks, seed, train_bs, train=True)
-    test_loader = make_loader(num_tasks, seed, test_bs, train=False)
-    
-    # Optimizer and Loss
-    optimizer = torch.optim.Adam(model.parameters(), lr=3e-6, weight_decay=0)
-    criterion = nn.CrossEntropyLoss()
-    
-    for curr_task in range(num_tasks):
-        train_loader.sampler.set_active_tasks(curr_task)
-        for e in tqdm(range(num_epochs)):
-            model.train()
-            for batch_idx, (imgs, targets) in enumerate(train_loader):
-                optimizer.zero_grad()
-                imgs, targets = imgs.to(device), targets.to(device)
-                imgs = imgs.flatten(start_dim=1)
-                output = model(imgs)
-                pred = output.data.max(1, keepdim=True)[1]
-                train_loss = criterion(output, targets)
-                train_loss.backward()
-                # print(f"train_loss: {train_loss.item()}")
-                optimizer.step()
-                
-        model.eval()
-        correct = 0
-        with torch.no_grad():
-            for t in range(curr_task+1):
-                test_loader.sampler.set_active_tasks(t)
-                for imgs, targets in test_loader:
+    for seed in range(10):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = ModifiedInitMLP(**conf)
+        model = model.to(device)
+
+        train_loader = make_loader(num_tasks, seed, train_bs, train=True)
+        test_loader = make_loader(num_tasks, seed, test_bs, train=False)
+
+        # Optimizer and Loss
+        optimizer = torch.optim.Adam(model.parameters(), lr=3e-6, weight_decay=0)
+        criterion = nn.CrossEntropyLoss()
+
+        # records latest task's test accuracy
+        single_acc = []
+        for curr_task in range(num_tasks):
+            train_loader.sampler.set_active_tasks(curr_task)
+            for e in range(num_epochs):
+                model.train()
+                for batch_idx, (imgs, targets) in enumerate(train_loader):
+                    optimizer.zero_grad()
                     imgs, targets = imgs.to(device), targets.to(device)
                     imgs = imgs.flatten(start_dim=1)
                     output = model(imgs)
                     pred = output.data.max(1, keepdim=True)[1]
-                    correct += pred.eq(targets.data.view_as(pred)).sum().item()
-            print(f"correct: {correct}")
-            acc = 100. * correct * num_tasks / (curr_task+1) / len(test_loader.dataset)
-            print(f"[t:{t} e:{e}] test acc: {acc}%")
+                    train_loss = criterion(output, targets)
+                    train_loss.backward()
+                    # print(f"train_loss: {train_loss.item()}")
+                    optimizer.step()
 
+            model.eval()
+            total_correct = 0
+            with torch.no_grad():
+                for t in range(curr_task+1):
+                    latest_correct = 0
+                    test_loader.sampler.set_active_tasks(t)
+                    for imgs, targets in test_loader:
+                        imgs, targets = imgs.to(device), targets.to(device)
+                        imgs = imgs.flatten(start_dim=1)
+                        output = model(imgs)
+                        pred = output.data.max(1, keepdim=True)[1]
+                        latest_correct += pred.eq(targets.data.view_as(pred)).sum().item()
+                    total_correct += latest_correct
+                    # record latest trained task's test acc
+                    if t == curr_task:
+                        # hardcoded number of test examples per mnist digit/class
+                        single_acc.append(100 * latest_correct / 10000)
+                # print(f"correct: {correct}")
+                acc = 100. * total_correct * num_tasks / (curr_task+1) / len(test_loader.dataset)
+                print(f"[t:{t} e:{e}] test acc: {acc}%")
+
+        print("single accuracies: ", single_acc)
+                
     print("SCRIPT FINISHED!")
