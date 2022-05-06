4361d2553247fd19b1f4462e61105e5488f33a78
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-05 21:09:09 -0700

    Update adn_10t.py

--- adn_10t.py
+++ adn_10t.py
@@ -29,7 +29,7 @@ conf = dict(
 )    
 
 if __name__ == "__main__":
-    seeds = range(10) # [33, 34, 35, 36, 37]
+    seeds = range(1) # [33, 34, 35, 36, 37]
     # used for creating avg over all seed runs
     all_single_acc = []
     all_avg_acc = []
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 
         # @TODO use Euclidian distance to infer which task's input at test time
         # calculate all the context vectors, avg's of each tasks' inputs
-        """
+        #"""
         contexts = []
         for curr_task in range(num_tasks):
             train_loader.sampler.set_active_tasks(curr_task)
@@ -59,7 +59,7 @@ if __name__ == "__main__":
             # hardcoded for mnist train
             avg_task_input = _sum / 6000
             contexts.append(avg_task_input)
-        """
+        #"""
         
         # records latest task's test accuracy
         single_acc = []
@@ -71,8 +71,8 @@ if __name__ == "__main__":
                 for batch_idx, (imgs, targets) in enumerate(train_loader):
                     optimizer.zero_grad()
                     imgs, targets = imgs.to(device), targets.to(device)
-                    # context = contexts[curr_task]
-                    context = torch.zeros([784])
+                    context = contexts[curr_task]
+                    #context = torch.zeros([784])
                     context = context.to(device)
                     context = context.unsqueeze(0)
                     context = context.repeat(imgs.shape[0], 1)
@@ -92,8 +92,8 @@ if __name__ == "__main__":
                     test_loader.sampler.set_active_tasks(t)
                     for imgs, targets in test_loader:
                         imgs, targets = imgs.to(device), targets.to(device)
-                        # context = contexts[t]
-                        context = torch.zeros([784])
+                        context = contexts[t]
+                        # context = torch.zeros([784])
                         context = context.to(device)
                         context = context.unsqueeze(0)
                         context = context.repeat(imgs.shape[0], 1)
