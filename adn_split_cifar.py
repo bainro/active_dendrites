18d86468ec8e473aeb5d4572c390983fda1f2e13
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-05-11 16:34:14 -0700

    Update adn_split_cifar.py

--- adn_split_cifar.py
+++ adn_split_cifar.py
@@ -89,9 +89,9 @@ class LeNet5(nn.Module):
     
 def train(seed, train_bs, lr,):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = LeNet5(device, num_classes=2)
+    model = LeNet5(device, num_classes=10)
     model = model.to(device)
-    backup = LeNet5(device, num_classes=2)
+    backup = LeNet5(device, num_classes=10)
     backup = backup.to(device)
     
     train_loaders = make_loaders(seed, train_bs, train=True)
